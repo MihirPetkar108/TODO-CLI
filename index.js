e4850e58cb0ada93577ce4fe69ad5e0380f305d8
MihirPetkar108 <mihirpetkar2006@gmail.com>
2026-03-20 20:41:06 +0530

    Added Write command

--- index.js
+++ index.js
@@ -8,4 +8,53 @@ program
     .description("CLI for your daily TODO's")
     .version("0.0.0");
 
+program
+    .command("write")
+    .description("Add a todo!")
+    .argument("<string>", "What do you want to add?")
+    .action((content) => {
+        fs.readFile("todo.txt", "utf-8", (err, data) => {
+            if (err) {
+                if (err.code === "ENOENT") {
+                    data = "";
+                } else {
+                    console.log(
+                        "Error while reading the file during add to task!",
+                    );
+                    return;
+                }
+            }
+
+            let todos = [];
+
+            if (data) {
+                try {
+                    todos = JSON.parse(data);
+                } catch (e) {
+                    todos = [];
+                }
+            }
+
+            let newtodo = {
+                id: todos.length + 1,
+                task: content,
+            };
+
+            todos.push(newtodo);
+
+            fs.writeFile(
+                "todo.txt",
+                JSON.stringify(todos, null, 2),
+                "utf-8",
+                (err) => {
+                    if (err)
+                        console.log(
+                            "Error while writing to file during adding a task!",
+                        );
+                    else console.log("Task added successfully!");
+                },
+            );
+        });
+    });
+
 program.parse();
